c505b79d857301edcf8cb859cc5d9e54900bd717
Androz 2091 <androz2091@gmail.com>
2024-03-04 15:23:08 +0100

    add notif

--- index.js
+++ index.js
@@ -11,36 +11,49 @@ const writeCache = (newCache) => {
 };
 
 const resetCache = () => writeCache({
-    lastThreadId: 0
+    lastThreadId: 0,
+    lastNotificationId: 0
 });
 
 const edstemSynchronization = async () => {
 
-    fetch('https://eu.edstem.org/api/courses/1101/threads?limit=2&sort=new&filter=unresolved', {
+    const threads = (await (await fetch('https://eu.edstem.org/api/courses/1101/threads?limit=2&sort=new&filter=unresolved', {
         headers: {
             'X-Token': process.env.EDSTEM_TOKEN
         }
-    })
-    .then((data) => data.json())
-    .then((data) => {
-        let lastThread = data.threads[0];
-        console.log(data.threads)
-        if (lastThread.id !== cache.lastThreadId) {
-            sendNotification({
-                title: `CS-108 Nouveau post !`,
-                message: lastThread.title,
-                url: `https://edstem.org/eu/courses/1101/discussion/${lastThread.id}`,
-                url_title: 'Let\'s Edstem this post!',
-                priority: 0
-            });
-    
-            writeCache({
-                ...cache,
-                lastThreadId: lastThread.id
-            });
-        }
-    })
+    })).json()).threads;
+    let lastThread = threads[0];
+    if (lastThread.id !== cache.lastThreadId) {
+        sendNotification({
+            title: `CS-108 Nouveau post !`,
+            message: lastThread.title,
+            url: `https://edstem.org/eu/courses/1101/discussion/${lastThread.id}`,
+            url_title: 'Let\'s Edstem this post!',
+            priority: 0
+        });
+    }
 
+    const notifications = (await (await fetch('https://notes.eu.edstem.org/api/browser/before?id=1707056513', {
+        headers: {
+            'X-Token': process.env.EDSTEM_TOKEN
+        }
+    })).json()).notes;
+    let lastNotification = notifications[0];
+    if (lastNotification.id !== cache.lastNotificationId) {
+        sendNotification({
+            title: `CS-108 Notification`,
+            message: JSON.parse(lastNotification.body).thread.title,
+            url: `https://edstem.org/eu/courses/1101/discussion/${JSON.parse(lastNotification.body).thread.id}`,
+            url_title: 'Let\'s answer this guy!',
+            priority: 0
+        })
+    }
+
+    writeCache({
+        ...cache,
+        lastThreadId: lastThread.id,
+        lastNotificationId: lastNotification.id
+    });
 
 };
 
